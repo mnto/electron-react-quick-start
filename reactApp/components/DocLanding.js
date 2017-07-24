9fdd0f2e0743eb5a7820610f1b7e26fba5661489
oadeboye <oadeboye@wellesley.edu>
2017-07-24 11:18:55 -0700

    remove toolbar component

--- reactApp/components/DocLanding.js
+++ reactApp/components/DocLanding.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import Toolbar from './Toolbar';
 import TextEditor from './TextEditor';
+// import styles from '../assets/stylesheets/DocLanding'
 
 class DocLanding extends React.Component{
   render(){
     return(
-      <div>
-        <Toolbar />
+      <div className="docLanding">
         <TextEditor />
       </div>
     );
   }
-};
+}
