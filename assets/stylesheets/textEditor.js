7358d4d538f48ec56ca70d618f2f16c4f6a20035
everuthingaud <setiadar@bc.edu>
2017-07-24 13:51:09 -0700

    editor

---
+++ assets/stylesheets/textEditor.js
@@ -0,0 +1,17 @@
+const styles = {
+  editorRoot: {
+    background: #fff;
+    border: 1px solid #ddd;
+    font-family: 'Georgia', serif;
+    font-size: 14px;
+    padding: 15px;
+  },
+  editor: {
+    border-top: 1px solid #ddd;
+    cursor: text;
+    font-size: 16px;
+    margin-top: 10px;
+  }
+}
+
+export default style;
