c434e80db7a0fb11c950fa3b50fb61c8c501f00c
oadeboye <oadeboye@wellesley.edu>
2017-07-24 15:19:00 -0700

    Initial passport setup

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,26 @@ const mongoose = require('mongoose');
 const connect = process.env.MONGODB_URI || require('./models/connect');
 mongoose.connect(connect);
 
+//passport setup
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+import { User } from './models/models';
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 
 // Example route
 app.get('/', function (req, res) {
