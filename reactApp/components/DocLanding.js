9081de3b4504a7dec530d9a76c951ebe0e419f2a
oadeboye <oadeboye@wellesley.edu>
2017-07-24 11:52:29 -0700

    fixed linter issues, updated doc landing page

--- reactApp/components/DocLanding.js
+++ reactApp/components/DocLanding.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import TextEditor from './TextEditor';
-// import styles from '../assets/stylesheets/DocLanding'
+import styles from '../assets/stylesheets/docLanding.scss';
 
 class DocLanding extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
   render(){
     return(
       <div className="docLanding">
+        <a class="waves-effect waves-light btn"><i class="material-icons left">chevron_left</i>Back to Document Portal</a>
+        <div className="docInfo">
+          <h2>Document title!</h2>
+          <h4>Shareable ID: NUMBERS</h4>
+          <h6>Created by AUTHOR NAME on DATE</h6>
+        </div>
+        <div className="collaborators">Collaborators go here</div>
+        <button class="btn waves-effect waves-light" type="submit" name="action">
+          Save Changes
+          <i class="material-icons left">save</i>
+        </button>
         <TextEditor />
       </div>
     );
   }
 }
+
+export default DocLanding;
