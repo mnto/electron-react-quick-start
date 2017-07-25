32c4d37f2627635c3b28cbe0ec91c2c9dae9c6f7
everuthingaud <setiadar@bc.edu>
2017-07-24 17:13:41 -0700

    STYLE MAPPINAG COMPLETE

---
+++ reactApp/components/customMaps/alignStyleMap.js
@@ -0,0 +1,16 @@
+const alignStyleMap = {
+  'left': {
+    'display': 'block',
+    'text-align': 'left'
+  },
+  'right': {
+    'display': 'block',
+    'text-align': 'right'
+  },
+  'center': {
+    'display': 'block',
+    'text-align': 'center'
+  }
+};
+
+export default alignStyleMap;
