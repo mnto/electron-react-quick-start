de7539a1646affa8d766a141c55f02205a51bec3
everuthingaud <setiadar@bc.edu>
2017-07-25 14:46:51 -0700

    m

--- frontend/reactApp/components/Register.js
+++ frontend/reactApp/components/Register.js
@@ -59,20 +59,20 @@ class Register extends React.Component{
           <form className="col s12" onSubmit={() => this.onSubmit()} method="POST" action="/register">
             <div className="row">
               <div className="input-field col s6">
-                <label for="icon_prefix">Username</label>
                 <i className="material-icons prefix">account_box</i>
-                <input id="icon_prefix" type="text" className="validate" onChange={(e) => this.handleUsernameChange(e)} />
+                <input id="icon_prefix" type="text" className="validate" onChange={(e) => this.handleUsernameChange(e)}/>
+                <label for="icon_prefix">Username</label>
               </div>
               <div className="input-field col s6">
                 <label for="icon_telephone">Password</label>
                 <i className="material-icons prefix">lock</i>
-                <input id="icon_telephone" type="password" className="validate" onChange={(e) => this.handlePasswordChange(e)} />
+                <input id="icon_telephone" type="password" className="validate" onChange={(e) => this.handlePasswordChange(e)}/>
               </div>
             </div>
             <div>
+              Register
+              <i className="material-icons right">send</i>
               <input className="btn waves-effect waves-light green accent-3" type="submit" name="action"/>
-                Register
-                <i className="material-icons right">send</i>
             </div>
           </form>
            <a class="waves-effect btn-flat" href="/login">Back to login</a>
