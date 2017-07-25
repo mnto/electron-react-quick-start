bb1649feb746f0be79718145d69e53515aae31c6
Julia <jklugher@wellesley.edu>
2017-07-25 11:11:28 -0700

    new doc modal

---
+++ frontend/reactApp/components/NewDocModal.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Document } from '../../backend/models/models';
+
+class NewDocModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      password: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default NewDocModal;
