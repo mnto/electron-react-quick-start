9cb492e238a2da0fccf0f26a35e78e6f809520c8
oadeboye <oadeboye@wellesley.edu>
2017-07-25 15:35:42 -0700

    Fix register button

--- frontend/reactApp/components/Register.js
+++ frontend/reactApp/components/Register.js
@@ -56,9 +56,10 @@ class Register extends React.Component{
               </div>
             </div>
             <div>
-              Register
-              <i className="material-icons right">send</i>
-              <input className="btn waves-effect waves-light green accent-3" type="submit" name="action"/>
+              <button className="btn waves-effect waves-light green accent-3" type="submit" name="action">
+                <i className="material-icons right">send</i>
+                Register!
+              </button>
             </div>
           </form>
            <Link className="waves-effect btn-flat" to="/">Back to login</Link>
