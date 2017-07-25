899d33b3eee2e021b08a743ca3930e3d9d195092
oadeboye <oadeboye@wellesley.edu>
2017-07-25 11:09:05 -0700

    Update document portal component

---
+++ frontend/reactApp/components/DocPortal.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import axios from 'axios';
+import { Button } from 'react-materialize';
+import { Document } from '../../backend/models/models';
+
+class DocPortal extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      ownDocs: [],
+      collabDocs: []
+    };
+  }
+
+  componentDidMount(){
+    axios.get('/user/'+this.props.userId)
+    .then((response) => response.json())
+    .then(jsonResp => {
+      const ownDocs = jsonResp.ownDocs;
+      const collabDocs = jsonResp.collaborating;
+
+      this.setState({
+        ownDocs,
+        collabDocs
+      });
+    })
+    .catch((err) => {
+      console.log("error finding documents", err);
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <h4>Your documents</h4>
+        <ul>
+          {this.state.ownDocs.map(
+            (doc) => {
+              return <li><Button floating large node='a' href={'/documents/' + doc.id} waves='light' icon='insert_drive_file' />{doc.name}</li>;
+            }
+          )}
+        </ul>
+        <ul>
+          <h4>Collaborating documents</h4>
+          {this.state.collabDocs.map(
+            (doc) => {
+              return <li><Button floating large node='a' href={'/documents/' + doc.id} waves='light' icon='insert_drive_file' />{doc.name}</li>;
+            }
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default DocPortal;
