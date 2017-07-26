8bcc55118166b68d9debd166498ac76512f254e9
oadeboye <oadeboye@wellesley.edu>
2017-07-26 14:38:11 -0700

    move error handling socket

--- backend/socket.js
+++ backend/socket.js
@@ -5,13 +5,14 @@ module.exports = io.on('connection', (socket) => {
   socket.on('documentId', (documentId) => {
     room = documentId.toString();
     socket.join(room);
-    socket.on('errorMessage', (err) => {
-      console.log("ERROR", err);
-    });
   });
 
   socket.on('sendContentState', (cs) => {
     socket.to(room).emit('sendBackContentState', cs);
+  });
+
+  socket.on('errorMessage', (err) => {
+    console.log("ERROR", err);
   });
 
 });
